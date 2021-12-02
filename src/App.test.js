0cf75b0a5877b1ca3b79565a5806e9a2e8c2364f
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-12-02 21:06:39 +0300

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('should return the home page', () => {
+  const home = render(<App />);
+  expect(home).toMatchSnapshot();
+  expect(home.queryByText(/Bitcoin/)).toBeInTheDocument();
+})
+
+// I believe the test below is important because it ensures the output value exists.
+// Without the output value, the imputs are useless
+
+test('should return the home page', () => {
+  const home = render(<App />);
+  expect(home.queryByText(/0/)).toBeInTheDocument();
+})
+
