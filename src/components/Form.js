a3a02fc9e9e8ed56074f28b3de2809293f301e57
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-12-02 14:08:24 +0300

    fetch fiat equivalent of input number

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [state, setState] = useState(0);
+
   const handleClick = () => {
-    const params = {
-      convert: 'USD',
-      id: 1,
-      amount: 40,
-    };
+    const amount = document.getElementById('number').value ? document.getElementById('number').value : '0';
 
-    const api = new URL('https://radiant-retreat-45087.herokuapp.com/https://pro-api.coinmarketcap.com/v1/tools/price-conversion');
+    if (amount === '0') {
+      setState(() => 0);
+    } else {
+      const params = {
+        convert: 'USD',
+        id: 1,
+        amount,
+      };
 
-    Object.keys(params).forEach((key) => api.searchParams.append(key, params[key]));
+      const api = new URL('https://radiant-retreat-45087.herokuapp.com/https://pro-api.coinmarketcap.com/v1/tools/price-conversion');
 
-    fetch(api, {
-      headers: {
-        'X-CMC_PRO_API_KEY': '6a327d7f-9672-4415-93f1-d1cea0b90e17',
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => console.log(res.data.quote.USD.price));
+      Object.keys(params).forEach((key) => api.searchParams.append(key, params[key]));
+
+      fetch(api, {
+        headers: {
+          'X-CMC_PRO_API_KEY': '6a327d7f-9672-4415-93f1-d1cea0b90e17',
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => setState(() => res.data.quote.USD.price));
+    }
   };
 
   return (
     <div>
-      <input type="number" placeholder="Enter Amount" />
+      <input id="number" type="number" placeholder="Enter Amount" onChange={handleClick} />
       <br />
       <select name="crypto" id="crypto" onChange={handleClick}>
-        <option value="Bitcoin">Bitcoin (BTC)</option>
-        <option value="Etherium">Etherium (ETH)</option>
-        <option value="Litecoin">Litecoin (LIT)</option>
+        <option id="1" value="Bitcoin">Bitcoin (BTC)</option>
+        <option id="1" value="Etherium">Etherium (ETH)</option>
+        <option id="1" value="Litecoin">Litecoin (LIT)</option>
       </select>
       <button type="button">
         <i className="fa fa-exchange" aria-hidden="true" />
@@ -38,6 +46,7 @@ function Form() {
         <option value="Pound Sterlings">Pound Sterling (£)</option>
         <option value="Uganda Shillings">Uganda Shillings</option>
       </select>
+      <p>{ state }</p>
     </div>
   );
 }
