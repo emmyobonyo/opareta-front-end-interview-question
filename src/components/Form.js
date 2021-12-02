cd39a53b4caed2ccae1e6785928205d4e38eacb9
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-12-02 10:42:01 +0300

    Add proxy to fetch data from the API

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 function Form() {
+  const handleClick = () => {
+    const params = {
+      convert: 'USD',
+      id: 1,
+      amount: 40,
+    };
+
+    const api = new URL('https://radiant-retreat-45087.herokuapp.com/https://pro-api.coinmarketcap.com/v1/tools/price-conversion');
+
+    Object.keys(params).forEach((key) => api.searchParams.append(key, params[key]));
+
+    fetch(api, {
+      headers: {
+        'X-CMC_PRO_API_KEY': '6a327d7f-9672-4415-93f1-d1cea0b90e17',
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res.data.quote.USD.price));
+  };
+
   return (
     <div>
       <input type="number" placeholder="Enter Amount" />
       <br />
-      <select name="crypto">
+      <select name="crypto" id="crypto" onChange={handleClick}>
         <option value="Bitcoin">Bitcoin (BTC)</option>
         <option value="Etherium">Etherium (ETH)</option>
         <option value="Litecoin">Litecoin (LIT)</option>
@@ -13,7 +33,7 @@ function Form() {
       <button type="button">
         <i className="fa fa-exchange" aria-hidden="true" />
       </button>
-      <select name="currency">
+      <select name="currency" id="currency">
         <option value="US Dollars">US Dollars ($)</option>
         <option value="Pound Sterlings">Pound Sterling (£)</option>
         <option value="Uganda Shillings">Uganda Shillings</option>
